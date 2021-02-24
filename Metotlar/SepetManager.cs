f831acadbfb006948318e1cfe554c7b7a2a81954
CisemmAkmann <akmancisem45@gmail.com>
2021-02-24 16:37:12 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //manager  -- operasyon tutar.
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urun.Adi + " " + urun.StokAdedi + " tane");
+        }
+    }
+}
